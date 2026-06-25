d356564c8d07fde849800d34c28054ec19333076
Mohsen <Moohamadiann@gmail.com>
2026-06-25 18:17:26 +0330

    Add some new static vars

--- config.py
+++ config.py
@@ -9,7 +9,8 @@ class conf:
     SESSION_STR= os.getenv("SESSION_STR")
     CHANNEL_USERNAME= os.getenv("CHANNEL_USERNAME")
     DOWNLOAD_PATH=os.getenv("DOWNLOAD_PATH")
-    DATA_FILE_PATH = os.getenv("DATA_FILE_PATH")
+    ORIGIN_DATA_FILE_PATH = os.getenv("ORIGIN_DATA_FILE_PATH")
+    DOWNLOADED_DATA_FILE_PATH = os.getenv("DOWNLOADED_DATA_FILE_PATH")
     DATE_FORMAT = os.getenv("DATE_FORMAT")
     LOAD_START_DATE = os.getenv("LOAD_START_DATE")
     DURATION_LIMIT = int(os.getenv("DURATION_LIMIT"))
@@ -17,10 +18,14 @@ class conf:
     DATA_FETCH_LIMIT = int(os.getenv("DATA_FETCH_LIMIT"))
     DATA_FETCH_SIZE_LIMIT = os.getenv("DATA_FETCH_SIZE_LIMIT")
     FILENAME_PATTERN = os.getenv("FILENAME_PATTERN")
-    JSON_DATA_FORM = {
+    DOWNLOADED_JSON_DATA_FORM = {
                 "audio_info" : {},
                 "map_msg_id" : {},
                 "general_info": {
-                    "last_internal_id": 0
+                    "last_downloaded_internal_id": 0
                 }
             }
+    
+    REPO_OWNER = os.getenv("REPO_OWNER")
+    REPO_NAME = os.getenv("REPO_NAME")
+    REPO_BRANCH = os.getenv("REPO_BRANCH")
